7782fd2f7cc2e4c5e7b45ad9cce9ea498468b583
AbigailKozma <47406819+AbigailKozma@users.noreply.github.com>
2019-02-07 15:30:47 -0500

    Update Studnent.sql

--- Studnent.sql
+++ Studnent.sql
@@ -1,7 +1,10 @@
 create table Student(
   fname varchar(30) not null,
-  lname varchar(30) not null, 
-  primary key (fname, lname)
+  lname varchar(30) not null,
+  roomnum int,
+  primary key (fname, lname),
+  foreign key (roomnum) References Room (room_number)
+  
 );
 
 
@@ -10,3 +13,19 @@ create table Room(
   number_beds int not nuss,
   occupancy int
   );
+
+/*fill in students*/
+insert into Student ('harry', 'Potter','1');
+
+
+/*fill in Room*/
+insert into Room values ('1','2', );
+insert into Room values ('2','2', );
+insert into Room values ('3','2', );
+insert into Room values ('4','2', );
+insert into Room values ('5','2', );
+insert into Room values ('6','1', );
+insert into Room values ('7','1', );
+insert into Room values ('8','1', );
+insert into Room values ('9','1', );
+insert into Room values ('10','1', );
