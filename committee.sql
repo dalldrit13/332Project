07b97a807577c01628b629d12a925ce85d5bffb6
AbigailKozma <47406819+AbigailKozma@users.noreply.github.com>
2019-02-07 13:50:37 -0500

    Update committee.sql

--- committee.sql
+++ committee.sql
@@ -6,60 +6,60 @@ create table Committee_Member(
 );
 
 create table Subcommittee(
-  name varchar(20) not null primary key
+  name varchar(20) not null primary key,
   chair_fname varchar(30) not null, 
-  chair_lname varchar(3)) not null,
-  foriegn key (chair_fname, chair_lname) references Committee_Member (fname, lname)
+  chair_lname varchar(30) not null,
+  foreign key (chair_fname, chair_lname) references Committee_Member (fname, lname)
 );
 
 
 create table ismember(
-  firstname varchar(30),
-  lastname varchar(30),
-  committeename varchar(30),
-  foriegn key (firstname, lastname) references Committee_Member(fname, lname) on delete cascade,
-  foriegn key (comitteename) referneces Subcomittee(name) on delete cascade
+  firstname varchar(30) not null,
+  lastname varchar(30) not null,
+  committeename varchar(30) not null,
+  primary key(firstname, lastname, committeename),
+  foreign key (firstname, lastname) references Committee_Member(fname, lname) on delete cascade,
+  foreign key (committeename) references Subcommittee(name) on delete cascade
   
   );
 
 /* Comittee Member */
-insert info Committee_Member values ('Abigail','Kozma');
-insert info Committee_Member values ('Matt','Kruzich');
-insert info Committee_Member values ('Brad','Pitt');
-insert info Committee_Member values ('Devin','Alldrit');
-insert info Committee_Member values ('Tamara','Djukic');
-insert info Committee_Member values ('Maddie','Leranbaum');
-insert info Committee_Member values ('Molly','White');
-insert info Committee_Member values ('Sarah','Smith');
-insert info Committee_Member values ('Sydney','Wilson');
-insert info Committee_Member values ('Kevin','Glasek');
-insert info Committee_Member values ('Jacob','Glasek');
-insert info Committee_Member values ('Sam','Marchetti');
-insert info Committee_Member values ('Carie','Trimble');
-insert info Committee_Member values ('Jordan','Rogers');
+insert into Committee_Member values ('Abigail','Kozma');
+insert into Committee_Member values ('Matt','Kruzich');
+insert into Committee_Member values ('Brad','Pitt');
+insert into Committee_Member values ('Devin','Alldrit');
+insert into Committee_Member values ('Tamara','Djukic');
+insert into Committee_Member values ('Maddie','Leranbaum');
+insert into Committee_Member values ('Molly','White');
+insert into Committee_Member values ('Sarah','Smith');
+insert into Committee_Member values ('Sydney','Wilson');
+insert into Committee_Member values ('Kevin','Glasek');
+insert into Committee_Member values ('Jacob','Glasek');
+insert into Committee_Member values ('Sam','Marchetti');
+insert into Committee_Member values ('Carie','Trimble');
+insert into Committee_Member values ('Jordan','Rogers');
   
   
  /* SubComittee */
-insert info Subcommittee values ('Program','Abigail','Kozma');  
-insert info Subcommittee values ('Registration','Matt','Kruzich');  
-insert info Subcommittee values ('Sponsors','Devin','Alldrit');
-insert info Subcommittee values ('Room_Assignment','Tamara','Djukic');
-insert info Subcommittee values ('Finance','Molly','White');
+insert into Subcommittee values ('Program','Abigail','Kozma');  
+insert into Subcommittee values ('Registration','Matt','Kruzich');  
+insert into Subcommittee values ('Sponsors','Devin','Alldrit');
+insert into Subcommittee values ('Room_Assignment','Tamara','Djukic');
+insert into Subcommittee values ('Finance','Molly','White');
 
   
 /* is member */
-insert info ismember values ('Abigail','Kozma', 'Program');
-insert info ismember values ('Matt','Kruzich', 'Registration');
-insert info ismember values ('Brad','Pitt','Sponsors');
-insert info ismember values ('Devin','Alldrit','Sponsors');
-insert info ismember values ('Tamara','Djukic', 'Room_Assignment');
-insert info ismember values ('Maddie','Leranbaum',,'Sponsors');
-insert info ismember values ('Molly','White', 'Finance');
-insert info ismember values ('Sarah','Smith','Sponsors');
-insert info ismember values ('Sydney','Wilson','Finance');
-insert info ismember values ('Kevin','Glasek','Finance');
-insert info ismember values ('Jacob','Glasek','Sponsors');
-insert info ismember values ('Sam','Marchetti','Program');
-insert info ismember values ('Carie','Trimble','Program');
-insert info ismember values ('Jordan','Rogers','Sponsors');
-  
+insert into ismember values ('Abigail','Kozma', 'Program');
+insert into ismember values ('Matt','Kruzich', 'Registration');
+insert into ismember values ('Brad','Pitt','Sponsors');
+insert into ismember values ('Devin','Alldrit','Sponsors');
+insert into ismember values ('Tamara','Djukic', 'Room_Assignment');
+insert into ismember values ('Maddie','Leranbaum',,'Sponsors');
+insert into ismember values ('Molly','White', 'Finance');
+insert into ismember values ('Sarah','Smith','Sponsors');
+insert into ismember values ('Sydney','Wilson','Finance');
+insert into ismember values ('Kevin','Glasek','Finance');
+insert into ismember values ('Jacob','Glasek','Sponsors');
+insert into ismember values ('Sam','Marchetti','Program');
+insert into ismember values ('Carie','Trimble','Program');
+insert into ismember values ('Jordan','Rogers','Sponsors');
