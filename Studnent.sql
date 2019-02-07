0d306dc380db138cfbd02de300ae5104191eda00
AbigailKozma <47406819+AbigailKozma@users.noreply.github.com>
2019-02-07 15:07:51 -0500

    Update Studnent.sql

--- Studnent.sql
+++ Studnent.sql
@@ -1,3 +1,12 @@
 create table Student(
-
+  fname varchar(30) not null,
+  lname varchar(30) not null, 
+  primary key (fname, lname)
 );
+
+
+create table Room(
+  room_number int not null primary key,
+  number_beds int not nuss,
+  occupancy int
+  );
