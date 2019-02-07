b43dcc079e28084a7c3b53fa82023777fa741e8b
AbigailKozma <47406819+AbigailKozma@users.noreply.github.com>
2019-02-07 12:49:20 -0500

    Update committee.sql

--- committee.sql
+++ committee.sql
@@ -1,16 +1,24 @@
-create table Subcommittee(
-  name varchar(20) not null primary key
-  chairfname varchar(30) not null, 
-  chairlname varchar(3)) not null,
-  foriegn key (chairfname, chairlname) references Committee_Member (fname, lname)
-);
-
-
-
-create table Comimittee_Member(
+create table Committee_Member(
   fname varchar(30) not null,
   lname varchar(30) not null,
   primary key(fname,lname)
  
 );
+
+create table Subcommittee(
+  name varchar(20) not null primary key
+  chair_fname varchar(30) not null, 
+  chair_lname varchar(3)) not null,
+  foriegn key (chair_fname, chair_lname) references Committee_Member (fname, lname)
+);
+
+
+create table ismember(
+  firstname varchar(30),
+  lastname varchar(30),
+  committeename varchar(30),
+  foriegn key(firstname, lastname) references Committee_Member(fname, lname),
+  foriegn key(comitteename) referneces Subcomittee(name)
+  
+  );
 
